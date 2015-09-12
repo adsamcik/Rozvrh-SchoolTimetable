3e2b8cb2266cb8c66b6a3389a9d5e006de71b477
Adam Novák <adsamcik@gmail.com>
2015-09-12 08:06:13 +0200

    Sync

---
+++ Rozvrh/AddClassInstance.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Rozvrh {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddClassInstance : Page {
+        public AddClassInstance() {
+            this.InitializeComponent();
+            comboBoxClass.ItemsSource = Data.classes;
+
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+
+            string[] days = new string[5];
+
+            days[0] = loader.GetString("Monday");
+            days[1] = loader.GetString("Tuesday");
+            days[2] = loader.GetString("Wednesday");
+            days[3] = loader.GetString("Thursday");
+            days[4] = loader.GetString("Friday");
+
+            comboBoxDay.ItemsSource = days;
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e) {
+            Frame.Navigate(typeof(AddClass));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            if(Frame.CanGoBack)
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            else
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e) {
+            Data.classInstances.Add(new ClassInstance((Class)comboBoxClass.SelectedItem, timePickerFrom.Time, timePickerTo.Time, textBoxRoom.Text, (classes.WeekDay)comboBoxDay.SelectedIndex));
+            Frame.GoBack();
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e) {
+            Frame.GoBack();
+        }
+    }
+}
---
+++ Rozvrh/classes/ClassInstance.cs
@@ -0,0 +1,28 @@
+﻿using Rozvrh.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozvrh {
+    public class ClassInstance {
+        public Class classData;
+        public TimeSpan from, to;
+        public string room;
+        public WeekDay day;
+
+        public ClassInstance(Class classData, TimeSpan from, TimeSpan to, string room, WeekDay day) {
+            this.classData = classData;
+            this.from = from;
+            this.to = to;
+            this.room = room;
+            this.day = day;
+        }
+
+        public override string ToString() {
+            return classData.ToString() + " " + from.ToString() + " - " + to.ToString();
+        }
+
+    }
+}
